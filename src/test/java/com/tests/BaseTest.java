879b99a8c478ba2b74da46617c5526a4f6926312
Shank24 <shankkalra@gmail.com>
2020-07-02 18:34:15 +0530

    Adding Maven Updates

---
+++ src/test/java/com/tests/BaseTest.java
@@ -0,0 +1,50 @@
+package com.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    // Browser -> chrome/ff
+    // HUB_HOST -> localhost/10.0.1.3
+
+    @BeforeTest
+    public void setUp(ITestContext iTestContext) throws MalformedURLException {
+
+        //Default Value
+        String host="localhost";
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability("browserName","chrome");
+
+        //Value set based on provided
+        if(System.getProperty("BROWSER")!=null && System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
+            dc.setCapability("browserName","firefox");
+        }
+
+        if(System.getProperty("HUB_HOST")!=null){
+            host=System.getProperty("HUB_HOST");
+        }
+
+        String testName = iTestContext.getCurrentXmlTest().getName();
+
+        String completeUrl = "http://" + host + ":4444/wd/hub";
+
+        dc.setCapability("name",testName);
+        this.driver=new RemoteWebDriver(new URL(completeUrl),dc);
+
+    }
+
+    @AfterTest
+    public void quitBrowser(){
+        this.driver.quit();
+    }
+}
